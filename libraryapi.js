cc9ee3dca1d4d2b986324a89bb073af69a9c9a4b
Mornach <mornach@Mornachs-MacBook-Pro.local>
2023-05-11 07:34:41 +0100

    push

--- libraryapi.js
+++ libraryapi.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 // Create Express app
@@ -9,6 +10,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -26,154 +28,99 @@ const bookSchema = new mongoose.Schema({
 const Book = mongoose.model("Book", bookSchema);
 
 // API endpoints
-app.get("/api/books", (req, res) => {
-  Book.find({}, (err, books) => {
-    if (err) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
+app.get("/api/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
     res.json(books);
-  });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.get("/api/books/:id", (req, res) => {
+app.get("/api/books/:id", async (req, res) => {
   const bookId = req.params.id;
-  Book.findById(bookId, (err, book) => {
-    if (err) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const book = await Book.findById(bookId);
     if (!book) {
       return res.status(404).json({ error: "Book not found" });
     }
     res.json(book);
-  });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.post("/api/books", (req, res) => {
+app.post("/api/books", async (req, res) => {
   const { title, author } = req.body;
   const book = new Book({ title, author });
-  book.save((err, savedBook) => {
-    if (err) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const savedBook = await book.save();
     res.status(201).json(savedBook);
-  });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.put("/api/books/:id", (req, res) => {
+app.put("/api/books/:id", async (req, res) => {
   const bookId = req.params.id;
   const { title, author } = req.body;
-  Book.findByIdAndUpdate(
-    bookId,
-    { title, author },
-    { new: true },
-    (err, updatedBook) => {
-      if (err) {
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-      if (!updatedBook) {
-        return res.status(404).json({ error: "Book not found" });
-      }
-      res.json(updatedBook);
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(
+      bookId,
+      { title, author },
+      { new: true }
+    );
+    if (!updatedBook) {
+      return res.status(404).json({ error: "Book not found" });
     }
-  );
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.delete("/api/books/:id", (req, res) => {
+app.delete("/api/books/:id", async (req, res) => {
   const bookId = req.params.id;
-  Book.findByIdAndDelete(bookId, (err, deletedBook) => {
-    if (err) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const deletedBook = await Book.findByIdAndDelete(bookId);
     if (!deletedBook) {
       return res.status(404).json({ error: "Book not found" });
     }
     res.json(deletedBook);
-  });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // Search books by title or author
-app.get("/api/search", (req, res) => {
+app.get("/api/books/search", async (req, res) => {
   const { query } = req.query;
   const searchRegex = new RegExp(query, "i");
-  Book.find(
-    { $or: [{ title: searchRegex }, { author: searchRegex }] },
-    (err, results) => {
-      if (err) {
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-      res.json(results);
-    }
-  );
+  try {
+    const results = await Book.find({
+      $or: [{ title: searchRegex }, { author: searchRegex }],
+    });
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // Pagination support
-app.get("/api/books/page/:page", (req, res) => {
+app.get("/api/books/page/:page", async (req, res) => {
   const pageSize = 5;
   const currentPage = parseInt(req.params.page);
   const skipCount = (currentPage - 1) * pageSize;
-  Book.find({})
-    .skip(skipCount)
-    .limit(pageSize)
-    .exec((err, paginatedBooks) => {
-      if (err) {
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-      res.json(paginatedBooks);
-    });
-});
-
-// Sorting books
-app.get("/api/books/sort/:field", (req, res) => {
-  const field = req.params.field.toLowerCase();
-
-  const sortOptions = {
-    title: { title: 1 },
-    author: { author: 1 },
-  };
-
-  if (!sortOptions[field]) {
-    return res.status(400).json({ error: "Invalid sorting field" });
-  }
-
-  Book.find({})
-    .sort(sortOptions[field])
-    .exec((err, sortedBooks) => {
-      if (err) {
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-      res.json(sortedBooks);
-    });
-});
-
-// Authentication middleware
-const authenticate = (req, res, next) => {
-  const apiKey = req.headers["x-api-key"];
-
-  if (!apiKey || apiKey !== process.env.API_KEY) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  next();
-};
-
-// Apply authentication middleware to specific routes
-app.get("/api/books/secure", authenticate, (req, res) => {
-  Book.find({}, (err, books) => {
-    if (err) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const books = await Book.find({}).skip(skipCount).limit(pageSize);
     res.json(books);
-  });
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Internal Server Error" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
